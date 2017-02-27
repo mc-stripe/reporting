f12b7d8845dd7159389b41837c357cb9e67b3cef
mc-stripe <mc@stripe.com>
2017-02-27 09:49:53 -0800

    Create complete_deal_signed.sql

---
+++ complete_deal_signed.sql
@@ -0,0 +1,75 @@
+-- country detail mapping
+with country_code as(
+select * from usertables.mc_country_codes_csv),
+-- team mapping
+team_role as(
+select * from usertables.mc_team_role_csv)
+
+select
+'deal_data' as data_type,
+-- deal_signed stats
+to_char(date_trunc('quarter', opportunity_close_date), 'YYYY-MM') as close_quarter,
+to_char(date_trunc('week', opportunity_close_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') as close_finance_week,
+-- pipeline created date
+to_char(date_trunc('quarter', opportunity_created_date), 'YYYY-MM') as pipline_created_quarter,
+to_char(date_trunc('week', opportunity_created_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') as pipeline_created_finance_week,
+-- expected go live dates
+to_char(date_trunc('quarter', opportunity_expected_go_live_date), 'YYYY-MM') as expected_go_live_quarter,
+to_char(date_trunc('week', opportunity_expected_go_live_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') as expected_go_live_finance_week,
+opportunity_expected_go_live_date,
+-- reference data
+cc.sales_region as region,
+cc.sfdc_country_name as country,
+'' as sales_channel,
+case
+  -- 1. filter team type first
+  --when sales_location = 'Hub' then 'Hub' 
+  when role = 'NBA' then 'NBA'
+  -- UK verticals
+  when cc.sales_region = 'UK' and opportunity_industry in ('Ticketing & Events', 'Travel & Hosp') then 'Ticketing/Travel'
+  when cc.sales_region = 'UK' and opportunity_industry in ('Financial') then 'Financial Services'
+  when cc.sales_region = 'UK' and opportunity_industry in ('Healthcare', 'Professional Services', 'Other Services','B2B', 'B2C Software', 'Content', 'Other Software & Content', 'B2C (Software)', 'B2B (Software)', 'Real Estate') then 'Services, Software & Content'
+  when cc.sales_region = 'UK' and opportunity_industry in ('Fashion', 'Food & Bev', 'Manufacturing', 'Other Retail') then 'Retail'
+  when cc.sales_region = 'UK' and opportunity_industry in ('Government', 'EDU', 'Non-Profit', 'Utilities', 'Other Public Sector') then 'Public Sector'
+  
+  
+  
+  -- US/CA
+  when cc.sfdc_country_name = 'United States' and opportunity_industry in ('B2B', 'B2C Software', 'Content', 'Other Software & Content', 'B2C (Software)', 'B2B (Software)') then 'Software & Content'
+  when cc.sfdc_country_name = 'United States' and  opportunity_industry in ('Ticketing & Events', 'Financial', 'Healthcare', 'Professional Services', 'Other Services', 'Travel & Hosp', 'Real Estate') then 'Services'
+  when cc.sfdc_country_name = 'United States' and  opportunity_industry in ('Government', 'EDU', 'Non-Profit', 'Utilities', 'Other Public Sector') then 'Public Sector'
+  when cc.sfdc_country_name = 'United States' and  opportunity_industry in ('Fashion', 'Food & Bev', 'Manufacturing', 'Other Retail') then 'Retail'
+  when cc.sfdc_country_name = 'United States' and  opportunity_industry is null then 'No industry'
+  when cc.sfdc_country_name = 'Canada' then 'Canada'  
+  -- SouthernEU
+  when cc.sales_region = 'Southern EU' then cc.sfdc_country_name
+  -- NorthernEU
+  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('DE','AT','CH') then 'DACH'
+  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('BE','NL','LU') then 'BENELUX'
+  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('NO', 'FI', 'SE', 'DK', 'IS') then 'BENELUX'  
+  -- AU/NZ
+  when cc.sales_region = 'AU' then cc.sfdc_country_name
+  -- SG
+  when cc.sales_region = 'SG' then cc.sfdc_country_name
+  when cc.sales_region = 'New Markets' then cc.sfdc_country_name
+  -- IE
+  when cc.sales_region = 'IE' then cc.sfdc_country_name
+  else 'other'
+end AS sub_region, 
+  opportunity_owner as owner,
+  usr.role as sales_role,
+  usr.team AS sales_location,
+  'OPTI__' || opportunity as sales_merchant_id,
+ opportunity_name as merchant_name, 
+opportunity_stage,
+opportunity_probability,
+opportunity_amount,
+opportunity_probability*opportunity_amount as  wgted_opportunity_amount
+
+from sales.salesforce ss
+JOIN country_code as cc ON ss.opportunity_merchant_country = cc.sfdc_country_name
+JOIN team_role as usr ON usr.sales_owner = ss.opportunity_owner
+WHERE
+opportunity_stage <> 'Lost'
+
+--GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
