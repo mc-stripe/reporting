ab7fc3dc14a56d9b45caba0e2dde6c102a8204cf
mc-stripe <mc@stripe.com>
2017-03-08 09:26:10 +0000

    Update complete_deal_signed.sql

--- complete_deal_signed.sql
+++ complete_deal_signed.sql
@@ -1,3 +1,5 @@
+-- change date at the bottom to report date
+
 -- country detail mapping
 with country_code as(
 select * from usertables.mc_country_codes_csv),
@@ -10,13 +12,14 @@ select
 -- deal_signed stats
 to_char(date_trunc('quarter', opportunity_close_date), 'YYYY-MM') as close_quarter,
 to_char(date_trunc('week', opportunity_close_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') as close_finance_week,
+case when opportunity_stage in ('Live', 'Onboarding') then 1 else 0 end as closed,
 -- pipeline created date
 to_char(date_trunc('quarter', opportunity_created_date), 'YYYY-MM') as pipline_created_quarter,
 to_char(date_trunc('week', opportunity_created_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') as pipeline_created_finance_week,
 -- expected go live dates
 to_char(date_trunc('quarter', opportunity_expected_go_live_date), 'YYYY-MM') as expected_go_live_quarter,
 to_char(date_trunc('week', opportunity_expected_go_live_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') as expected_go_live_finance_week,
-opportunity_expected_go_live_date,
+to_char(opportunity_expected_go_live_date, 'YYYY-MM-DD') as opportunity_expected_go_live_date,
 -- reference data
 cc.sales_region as region,
 cc.sfdc_country_name as country,
@@ -32,8 +35,6 @@ case
   when cc.sales_region = 'UK' and opportunity_industry in ('Fashion', 'Food & Bev', 'Manufacturing', 'Other Retail') then 'Retail'
   when cc.sales_region = 'UK' and opportunity_industry in ('Government', 'EDU', 'Non-Profit', 'Utilities', 'Other Public Sector') then 'Public Sector'
   
-  
-  
   -- US/CA
   when cc.sfdc_country_name = 'United States' and opportunity_industry in ('B2B', 'B2C Software', 'Content', 'Other Software & Content', 'B2C (Software)', 'B2B (Software)') then 'Software & Content'
   when cc.sfdc_country_name = 'United States' and  opportunity_industry in ('Ticketing & Events', 'Financial', 'Healthcare', 'Professional Services', 'Other Services', 'Travel & Hosp', 'Real Estate') then 'Services'
@@ -44,9 +45,9 @@ case
   -- SouthernEU
   when cc.sales_region = 'Southern EU' then cc.sfdc_country_name
   -- NorthernEU
-  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('DE','AT','CH') then 'DACH'
-  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('BE','NL','LU') then 'BENELUX'
-  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('NO', 'FI', 'SE', 'DK', 'IS') then 'BENELUX'  
+  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('Germany','Austria','Switzerland') then 'DACH'
+  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('Belgium','Netherlands','Luxembourg') then 'BENELUX'
+  when cc.sales_region = 'Northern EU' and cc.sfdc_country_name in ('Norway', 'Finland', 'Sweden', 'Denmark', 'Iceland') then 'NORDICS'  
   -- AU/NZ
   when cc.sales_region = 'AU' then cc.sfdc_country_name
   -- SG
@@ -64,12 +65,14 @@ end AS sub_region,
 opportunity_stage,
 opportunity_probability,
 opportunity_amount,
-opportunity_probability*opportunity_amount as  wgted_opportunity_amount
+opportunity_probability*opportunity_amount as  wgted_opportunity_amount,
+case when to_char(date_trunc('week', opportunity_created_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') = '2017-02-26' then 1 else 0 end as created_this_week,
+case when to_char(date_trunc('week', opportunity_close_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') = '2017-02-26'  then 1 else 0 end as closed_this_week,
+case when to_char(date_trunc('week', opportunity_expected_go_live_date + '1 day'::interval)::date - '1 day'::interval,'YYYY-MM-DD') = '2017-02-26'  then 1 else 0 end as live_this_week
+
 
 from sales.salesforce ss
 JOIN country_code as cc ON ss.opportunity_merchant_country = cc.sfdc_country_name
 JOIN team_role as usr ON usr.sales_owner = ss.opportunity_owner
 WHERE
 opportunity_stage <> 'Lost'
-
---GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
