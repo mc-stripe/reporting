b8e1088128218ed58f6ecfb7f27c69c80d353229
mc-stripe <mc@stripe.com>
2017-05-03 09:13:37 +0100

    Create code_snippets.sql

---
+++ code_snippets.sql
@@ -0,0 +1,7 @@
+select * from usertables.mc_team_roles_csv
+--select distinct sales__owner, sales__owner_role from dim.merchants where sales__owner_role not like '%AE%'
+
+--insert into usertables.mc_team_roles_csv values ('Nikunj Handa', '', 'AE', 'In-country', 'TEMPORARY FIX'), ('Satya Tammareddy', '', 'AE', 'In-country', 'TEMPORARY FIX'); 
+-- insert into usertables.mc_team_roles_csv values ('Nikunj Handa', '', 'AE', 'In-country', 'TEMPORARY FIX'); 
+
+--delete from usertables.mc_team_roles_csv where sales_owner in ('Satya Tammareddy', 'Nikunj Handa')
